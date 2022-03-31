01d2c4700a9145aa7b570a4005ed75a886fe9e63
Niklas Sankell <niklas_san@gmail.com>
2022-03-31 11:57:11 +0200

    added print method recursivly

--- main.js
+++ main.js
@@ -2,26 +2,27 @@ var axios = require("axios");
 var $ = require("jquery");
 
 let button = $('#myButton');
-let div = $(".swap");
+let questionContainer = $(".questionContainer");
 
+let getData = async (url) => {
+    button.hide();
+    let response = await axios.get(url);
+    return response.data;
+}
 
 button.on("click", async () => {
-    div.toggle();
     let questionsObject = await getData("https://opentdb.com/api.php?amount=10");
     questionsArray = questionsObject.results;
     printQuestions(questionsArray, 0);
 });
-
-let getData = async (url) => {
-    let response = await axios.get(url);
-    return response.data;
-}
 
 let printQuestions = async (questionsArray, counter) => {
     if (counter < questionsArray.length) {
 
         let questionObject = questionsArray[counter];
         let question = `${questionObject.question}`;
+
+        questionContainer.append(`<p>${questionObject.question}</p>`)
 
         let alternatives = [`correct: ${questionObject.correct_answer}`];
 
@@ -30,12 +31,15 @@ let printQuestions = async (questionsArray, counter) => {
         });
 
         console.log(question);
-        console.log("hej")
 
         alternatives.forEach(answer => {
             console.log(answer);
         });
+
+        printQuestions(questionsArray, counter+1);
+    } else {
+        console.log("Back to beginning");
+        button.show();
     }
 
-    console.log("array: "+questionsArray.length);
 }
